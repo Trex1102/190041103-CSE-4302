f45a0b9cff242aa382c3b6766c76f0a91bc38800
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-07-14 23:17:53 +0600

    adding lab 06

---
+++ Lab 06/103_Task01.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "103_task01.h"
+#include <math.h>
+using namespace std;
+
+void Coordinate::display()
+{
+	cout<<"Abscissa: "<<x<<endl<<"Ordinate: "<<y<<endl;
+}
+
+float Coordinate::getDistance (Coordinate c)
+{	
+	float res = ((x-c.x)*(x-c.x)) + ((y-c.y) * (y-c.y));
+	res = sqrt(res);
+	return res;
+}
+
+float Coordinate::getDistance ()
+{
+	float res = (x*x) + (y*y);
+	res = sqrt(res);
+	return res;
+}
+
+void Coordinate::move_x(float val)
+{
+	x += val;
+}
+
+void Coordinate::move_y(float val)
+{
+	y += val;
+}
+
+void Coordinate::move(float val)
+{
+	move_x(val);
+	move_y(val);
+}
+
+bool Coordinate::operator > (Coordinate c)
+{
+	if(x > c.x) return true;
+	else return false;
+}
+
+bool Coordinate::operator < (Coordinate c)
+{
+	if(x < c.x) return true;
+	else return false;
+}
+
+bool Coordinate::operator >= (Coordinate c)
+{
+	if(x >= c.x) return true;
+	else return false;
+}
+
+bool Coordinate::operator <= (Coordinate c)
+{
+	if(x <= c.x) return true;
+	else return false;
+}
+
+bool Coordinate::operator == (Coordinate c)
+{
+	if(x == c.x) return true;
+	else return false;
+}
+
+bool Coordinate::operator != (Coordinate c)
+{
+	if(x != c.x) return true;
+	else return false;
+}
+
+Coordinate Coordinate::operator ++()
+{
+	return (Coordinate(++x , ++y));
+}
+
+Coordinate Coordinate::operator ++(int)
+{
+	return (Coordinate(++x , ++y));
+}
+Coordinate Coordinate::operator --()
+{
+	return (Coordinate(--x , --y));
+}
+
+Coordinate Coordinate::operator --(int)
+{
+	return (Coordinate(--x , --y));
+}
+
+
+int main()
+{
+	Coordinate a(2,3);
+	a.display();
+}
---
+++ Lab 06/103_Task01.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+class Coordinate{
+private:
+	float x, y;
+public:
+	Coordinate(float x= 0 ,float y = 0): x(x), y(y){}
+	~Coordinate(){}
+	void display ();
+	float getDistance (Coordinate c);
+	float getDistance ();
+	void move_x(float val);
+	void move_y(float val);
+	void move(float val);
+	bool operator > (Coordinate c);
+	bool operator < (Coordinate c);
+	bool operator >= (Coordinate c);
+	bool operator <= (Coordinate c);
+	bool operator == (Coordinate c);
+	bool operator != (Coordinate c);
+	Coordinate operator ++();
+	Coordinate operator ++(int);
+	Coordinate operator --();
+	Coordinate operator --(int);
+};	
