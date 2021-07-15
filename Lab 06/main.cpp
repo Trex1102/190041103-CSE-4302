58ef678eba12851ba71368276d0823661ccf4f47
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-07-15 09:10:25 +0600

    checked all tasks

--- Lab 06/main.cpp
+++ Lab 06/main.cpp
@@ -6,9 +6,15 @@
 
 int main()
 {
+	int s = 5 ,b ;
+	b = ++s;
+	cout<<b<<endl;
+	cout<<s<<endl;
 	Coordinate c1(1,2) , c2(2,1);
 	c1.display();
 	if(c1 == c2) cout<<"same"<<endl;
+	c2 = c1++;
+	c2.display();
 	ZooAnimal a;
 	a.setName("Alan");
 	a.setHeight(9);
@@ -21,4 +27,8 @@ int main()
 	int x =5;
 	a.operator--(x);
 	a.getInformation(); 
+	FLOAT f1(5.052);
+	int i = f1;
+	cout<<i<<endl;
+
 }
