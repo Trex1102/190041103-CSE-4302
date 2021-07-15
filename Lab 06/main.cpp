e63316758456a15c6a1de0f37d4a3f24bfa46be1
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-07-15 08:40:55 +0600

    task 3 added

--- Lab 06/main.cpp
+++ Lab 06/main.cpp
@@ -2,10 +2,23 @@
 #include <math.h>
 #include "103_task01.cpp"
 #include "103_task02.cpp"
+#include "103_task03.cpp"
 
 int main()
 {
 	Coordinate c1(1,2) , c2(2,1);
 	c1.display();
-	if(c1 == c2) cout<<"same"<<endl; 
+	if(c1 == c2) cout<<"same"<<endl;
+	ZooAnimal a;
+	a.setName("Alan");
+	a.setHeight(9);
+	a.setWeight(120.3);
+	a.setBYear(2010);
+	a.setCageNumber(123);
+	a.getInformation();
+	cout<<a.getAge()<<endl;
+	cout<< (a==120)<<endl;
+	int x =5;
+	a.operator--(x);
+	a.getInformation(); 
 }
