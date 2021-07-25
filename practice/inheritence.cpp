23b9d06909276020ac816c9801caef6b74015b40
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-07-25 19:18:53 +0600

    virtual function

--- practice/inheritence.cpp
+++ practice/inheritence.cpp
@@ -6,7 +6,7 @@ class Animal // base class
 private:
 	int x;
 public:
-	Animal(int x):x(x){}
+	Animal(int x= 0):x(x){}
 	friend void setx(int y, Animal &a);
 	//void setx(int y) {x = y;}
 	void getx(){cout<<x<<endl;}
@@ -22,7 +22,7 @@ class Dog : public Animal{ // derived class with access specifier
 private:
 	int x;
 public:
-	Dog(int x):Animal(x), x(x){}
+	Dog(int x= 0):Animal(x), x(x){}
 	//void getx(){cout<<x<<endl;}
 	void eat(){ // overridding
 		Animal::eat(); // calling base class function using scope resulation operator
@@ -95,6 +95,54 @@ public:
 	virtual void draw() const {cout<<"This is circle"<<endl;}
 };
 
+
+//when to use:
+// to make a body of the project
+
+//pointer to derived class
+class Base{
+public:
+	int base_var;
+
+	virtual void display(){
+		cout<<"This is Base class"<<endl;
+		cout<<"Base Var: "<<base_var<<endl; 
+	}
+
+	void test(){cout<<"testing"<<endl;}
+};
+
+class Derived:public Base{
+public:
+	int derived_var;
+
+	void display(){
+		cout<<"This is Derived Class"<<endl;
+		cout<<"Derived Var: "<<derived_var<<endl;
+		cout<<"Base Var: "<<base_var<<endl; 
+	}
+};
+
+//virtual function: polymorphism
+// 1. A virtual function is a member function which is declared within a base class and is re-
+// defined(Overriden) by a derived class. When you refer to a derived class object using a pointer or a
+// reference to the base class, you can call a virtual function for that object and execute the derived
+// class’s version of the function.
+
+// 1. virtual function ensures the correct function is called regerdless of the pointer
+// 2. used to achieve runtime polymorphism --> non virtual compile time
+// 3. declared with virtual keyword
+// 4. cannot be static
+// 5. can be a friend function to another class
+// 6. should be accessed using pointer or reference of base class to acheive runtime 
+// polymorphism
+// 7. prototype should be same
+// 8. may have virtual destructor
+// 9. always defiend in the base class 
+
+
+
+
 int main()
 {
 	
@@ -107,4 +155,26 @@ int main()
 
 	Circle circle; // abstract class
 	circle.draw(); // pure virtual function
+
+	Base *base;
+	Derived *derived;
+	base = new Derived; // derived = base + something new
+	base->base_var = 5;
+	base ->display(); // showing the derived display --> 
+	base -> test();
+	derived = new Derived;
+	derived ->base_var = 6; // calling base class var from derived class pointer
+	derived ->derived_var = 7;
+	derived -> display();
+	derived -> test(); // calling base class function from derived class pointer
+
+	//conclusion : 
+	// A pointer to derived class is a pointer of base class pointing to derived class, but it will hold its aspect.
+	// This pointer of base class will be able to temper functions and variables of its own class and can still point to derived class object.
+
+	Base* bass = new Base;
+	bass->base_var = 10;
+	bass -> display();
+
+
 }
