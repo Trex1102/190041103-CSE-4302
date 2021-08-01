7acd71ccafc6fcbe30480ef8827e9b513fa7074c
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-08-02 00:23:21 +0600

    ostream

---
+++ practice/ostream.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+// for flush - https://www.geeksforgeeks.org/buffer-flush-means-c/
+
+void functions(){
+	char c = 'a';
+	cout<<"printing a\n";
+	cout.flush(); // ensure cout is written as it outputs from buffer directly
+	cout.put(c);
+}
+
+int main()
+{
+	functions();
+}
