ed218a2cf5ba8fa7336af21074b3842ff5468cee
saikat--osiris <tanvirhossain3@iut-dhaka.edu>
2021-08-26 01:02:36 +0600

    complete

---
+++ Lab 10/103_task02.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+private:
+    int data;
+public:
+    base(){}
+    base(int _data){
+        data = _data;
+    }
+    void display(){cout<<data<<endl;}
+    base operator = (base & x){
+        this->data = x.data;
+        cout<<"\nAssignment operator"<<endl;
+        return *this;
+    }
+    // User defined copy constructor
+    /*base (base &x){
+        data = x.data;
+        cout<<"User defined Copy constructor"<<endl;
+    }*/ 
+
+    ~base(){}
+};
+
+class magma
+{
+private:
+    int* add;
+public:
+    magma(){}
+    magma(int *x){
+        add =x;
+    }
+    void display(){
+        cout<<*add<<endl;
+        cout<<add<<endl;
+    }
+    magma operator=(magma &x){
+        add = new int;
+        *(add) = *(x.add);
+    }
+};
+
+int main()
+{
+    base b1(3);
+    base b2;
+    b2 = b1; // assignment operator
+    b2.display();
+    //base b3 = b2; // copy initialization  
+    base b3(b2); // default copy constructor
+    b3.display();
+    int n = 5;
+    magma m1(&n);
+    m1.display();
+    magma m2;
+    m2 = m1;
+    m2.display();
+
+
+}
